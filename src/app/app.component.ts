1e5069b0e0e46ba99b5d6ff4466f67859d14c883
DAVV422 <diegoalberto42216@gmail.com>
2023-09-21 16:41:59 -0400

    Flor Amarilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public contador: number = 0;
   title = 'flores_amarillas';
+  messages: string[] = ['Hola mi niña bonita', 'Se que no es mucho...', 'pero no dejaría que...',
+                        'pase este día...', 'sin darte tus...', 'Florecitas Amarillas', 'Te amo mi Amor ♥'];
+  public fin: number = this.messages.length - 1;
+  message: string = this.messages[this.contador];
+  count(){
+    this.contador++;
+    if(this.contador== this.fin + 1){
+      this.contador = 0;
+    }
+    this.message = this.messages[this.contador];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FlowerComponent } from './components/flower/flower.component';
+import { GifComponent } from './components/gif/gif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlowerComponent,
+    GifComponent
   ],
   imports: [
     BrowserModule
